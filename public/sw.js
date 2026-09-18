41a58cdb37a36f6404eab4a54f76d34a1ef713cb
hirakawa659 <hirakawa.novel@gmail.com>
2026-09-18 09:13:03 +0900

    feat: add PWA manifest and icons
    Configure PWA support by adding a manifest file and updating the index HTML to include the manifest and required mobile metadata.

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'novel-editor-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
